f110d1cb455fd8933bb5d819ad278170d9a74725
jam50tak <110869986+jam50tak@users.noreply.github.com>
2022-08-13 19:21:59 +0900

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="todoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のtodo</p>
+        <ul>
+          <div>
+            <li>あああ</li>　<button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいい</li>　<button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のtodo</p>
+        <ul>
+          <div>
+            <li>あああ</li>　<button>戻す</button>
+          </div>
+          <div>
+            <li>いいい</li>　<button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
